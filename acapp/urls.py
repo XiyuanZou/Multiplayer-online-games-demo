fb57d3664678f5a72d9bd34ed980c385ac6f7bf2
Xiyuan <3407381697@qq.com>
2022-02-17 01:14:39 +0800

    write html js views urls

--- acapp/urls.py
+++ acapp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    #path("", include("Game.urls")),
+    path('', include('Game.url.index')),
     path('admin/', admin.site.urls),
 ]
